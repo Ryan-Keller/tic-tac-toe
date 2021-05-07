e48f396f96a2cf2b310b5250d43879c3cfa17c63
Ryan Keller <rjkeller_01@hotmail.com>
2021-05-07 13:06:53 -0700

    added some javascript to swap turns

---
+++ app.js
@@ -0,0 +1,38 @@
+const CIRCLE_CLASS = 'circle'
+const X_CLASS = 'x'
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+let circleTurn
+
+cellElements.forEach(cell =>{
+    cell.addEventListener('click', handleClick, {once: true })
+})
+
+function handleClick(e){
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    // check for win
+    // check for draw
+    // switch turns
+    swapTurns()
+    setBoardHoverClass()
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass)
+}
+
+function swapTurns(){
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass(){
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if(circleTurn){
+        board.classList.add(CIRCLE_CLASS)
+    }else {
+        board.classList.add(X_CLASS)
+    }
+}
